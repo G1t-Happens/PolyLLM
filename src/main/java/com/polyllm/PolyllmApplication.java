a4d45faa23f892cf14917268a13a47be1c862d52
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2025-08-31 12:48:11 +0200

    Initial commit

---
+++ src/main/java/com/polyllm/PolyllmApplication.java
@@ -0,0 +1,13 @@
+package com.polyllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyllmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyllmApplication.class, args);
+	}
+
+}
