9bf71f9b49dd1b6d4a8a8b8fe6b695dee6ee54e7
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2025-08-31 17:40:33 +0200

    add: default Repository Interfaces

---
+++ src/main/java/com/polyllm/repository/ConversationRepository.java
@@ -0,0 +1,9 @@
+package com.polyllm.repository;
+
+import com.polyllm.entities.Conversation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ConversationRepository extends JpaRepository<Conversation, UUID> {
+}
